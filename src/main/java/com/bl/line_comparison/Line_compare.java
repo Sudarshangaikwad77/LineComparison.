6569bbdd46f49da05bcafe7e8aad941c167ac9d4
Sudarshan Gaikwad <sudarshangaikwad@gmailcom>
2023-04-17 14:13:44 +0530

    code added to UC2 branch

--- src/main/java/com/bl/line_comparison/Line_compare.java
+++ src/main/java/com/bl/line_comparison/Line_compare.java
@@ -20,5 +20,22 @@ public class Line_compare {
 
         System.out.println("The Length of Line Is: " + length);
 
+
+        //for uc2
+        System.out.print("Enter X1 Value: ");
+        double a1 = ab.nextDouble();
+        System.out.print("Enter Y1 Value: ");
+        double b1 = ab.nextDouble();
+        System.out.print("Enter X2 Value: ");
+        double a2 = ab.nextDouble();
+        System.out.print("Enter Y2 Value: ");
+        double b2 = ab.nextDouble();
+
+        /* Calculating the Line 2 Length UC-2 */
+
+        double length2 = Math.sqrt((Math.pow(a2 - a1, 2) + Math.pow(b2 - b1, 2)));
+
+        System.out.println("The Length of Line 2 is: " + length2);
+
     }
 }
